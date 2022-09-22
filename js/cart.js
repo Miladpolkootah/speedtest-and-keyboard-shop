7ede5c41f5027d89eba85fce452161bf91cfde9a
Milad Polkootah <miladpol723@gmail.com>
2022-09-22 13:34:36 +0330

    the new styles add to the slider and the cart and speedtest fixed

--- js/cart.js
+++ js/cart.js
@@ -159,32 +159,13 @@ function displayCart() {
   var output = "";
   for (var i in cartArray) {
     output +=
-      "<tr>" +
-      "<td>" +
-      cartArray[i].name +
-      "</td>" +
-      "<td>(" +
-      cartArray[i].price +
-      ")</td>" +
-      "<td><div class='input-group'><button class='minus-item input-group-addon btn btn-primary' data-name=" +
-      cartArray[i].name +
-      ">-</button>" +
-      "<input type='number' class='item-count form-control' data-name='" +
-      cartArray[i].name +
-      "' value='" +
-      cartArray[i].count +
-      "'>" +
-      "<button class='plus-item btn btn-primary input-group-addon' data-name=" +
-      cartArray[i].name +
-      ">+</button></div></td>" +
-      "<td><button class='delete-item btn btn-danger' data-name=" +
-      cartArray[i].name +
-      ">X</button></td>" +
-      " = " +
-      "<td>" +
-      cartArray[i].total +
-      "</td>" +
-      "</tr>";
+      `<div class='cartitems'><div>${cartArray[i].name}</div><div>(${cartArray[i].price})</div>
+      <div><div class='input-group'><button class='minus-item input-group-addon btn btn-primary' data-name="${cartArray[i].name}">-</button>
+      <input type="number" class='item-count' data-name="cartArray[i].name" value='${cartArray[i].count}'></input>
+      <button class='plus-item btn btn-primary input-group-addon' data-name="${cartArray[i].name}">+</button></div></div>
+      <div>${cartArray[i].total}</div>
+      <div><button class='delete-item btn btn-danger' data-name="${cartArray[i].name}">X</button></div>
+      </div>`;
   }
   $(".show-cart").html(output);
   $(".total-cart").html(shoppingCart.totalCart());
